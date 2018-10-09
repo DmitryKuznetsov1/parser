a0412e00a6059cf88cdc0367f0beb26c90793814
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-09 17:35:26 +0300

    first commit

---
+++ parser.py
@@ -0,0 +1,90 @@
+from requests import get, Session
+from bs4 import BeautifulSoup
+from time import sleep
+import requests
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
+              'Gecko/20100101 Firefox/50.0'
+      }
+# proxies = {
+#     'http': 'socks5://roskomnadzor:Ug9dp9mpHscK4NtKsUgV@94.130.184.58:1080',
+#     'https': 'socks5://roskomnadzor:Ug9dp9mpHscK4NtKsUgV@94.130.184.58:1080'
+# }
+proxies = {
+  'http': 'http://108.61.189.79:3128',
+  'https': 'https://108.61.189.79:3128'
+}
+def get_href(x):
+    return x.get('href')
+def get_href_2(x):
+    return x.a.get('href')
+
+# s = requests.Session()
+# data = {"login_username":"Dmitry_Kuznetsov", "login_password":"dmitrykuznetsov1998"}
+# url_auth = "http://kinopoisk.ru"
+# r = s.post(url_auth, data=data)
+# s.cookies
+
+main_url = 'https://rutracker.org'
+r = get(main_url)
+
+
+soup = BeautifulSoup(r.text, 'lxml')
+_ = soup.find('div', {'class': 'tr_main_cats'})
+category_list = _.findAll('a')
+category_list = list(map(get_href, category_list))
+print()
+#
+counter = 0
+#
+
+for i in category_list:
+    counter += 1
+    category_url = f'http://rutracker.org/forum/{i}'
+    c = open('nashe.html')
+    soup = BeautifulSoup(c, 'lxml')
+    _ = soup.find('table', {'class': 'forumline forum'})
+    subcategory_list = _.findAll('h4', {'class': 'forumlink'})
+    subcategory_list = [x.a for x in subcategory_list]
+    subcategory_list = list(map(get_href, subcategory_list))
+    print(subcategory_list)
+    for j in subcategory_list:
+        subcategory_url = f'http://rutracker.org/forum/{j}'
+        t = get(subcategory_url)
+        soup = BeautifulSoup(t, 'lxml')
+
+
+    if counter == 1:
+        break
+
+
+
+
+
+
+
+
+
+
+# #
+# counter = 0
+# #
+# for el in els:
+#     movie_href_list = el.findAll(href=True)
+#     movie_href = movie_href_list[1].get('href')
+#     movie_url = 'https://www.kinopoisk.ru%s' % (movie_href)
+#     rr = get(movie_url)
+#     sleep(5)
+#     #
+#     counter += 1
+#     if counter == 3:
+#         break
+#     #
+#     print(rr)
+#     soup = BeautifulSoup(rr.text, 'lxml')
+#     russian_name_ = soup.find('div', {'data-metrika': 'film_card'})
+#     russian_name = russian_name_.find('h1').text
+#     print(russian_name, counter)
+# with open('test1.html', 'w', encoding='UTF-8') as output_file:
+#     output_file.write(rr.text)
+#
