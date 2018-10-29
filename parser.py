ad54dd796c468e6b1bc47d1cc30286ab18453d3b
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-29 18:50:03 +0300

    get subcategory href

--- parser.py
+++ parser.py
@@ -25,11 +25,9 @@ r = get(main_url)
 
 soup = BeautifulSoup(r.text, 'lxml')
 _ = soup.find('div', {'class': 'tr_main_cats'})
-# category_list = _.findAll('a')
-# category_list = list(map(get_href, category_list))
-# Фильмы, Наше кино, Артхаус и авторское кино, DVD, HD, Док. фильмы, мультсериалы соответсвенно
+# Фильмы, Наше кино, Артхаус и авторское кино, DVD, HD, Мультсериалы соответсвенно
 category_list = ["viewforum.php?f=7", "viewforum.php?f=22", "viewforum.php?f=124", "viewforum.php?f=93",
-                 "viewforum.php?f=2198", "viewforum.php?f=46", "viewforum.php?f=4"]
+                 "viewforum.php?f=2198", "viewforum.php?f=4"]
 
 for i in category_list:
     category_url = 'http://rutracker.org/forum/%s' % (i)
@@ -37,7 +35,7 @@ for i in category_list:
     soup = BeautifulSoup(c.text, 'lxml')
     _ = soup.find('table', {'class': 'forumline forum'})
     subcategory_list = _.findAll('h4', {'class': 'forumlink'})
-    subcategory_list = [x.text for x in subcategory_list]
+    subcategory_list = [x.get('href') for x in subcategory_list]
     # subcategory_list = list(map(get_href, subcategory_list))
     print(subcategory_list)
     # for j in subcategory_list:
@@ -46,7 +44,4 @@ for i in category_list:
     #     soup = BeautifulSoup(t, 'lxml')
     # with open('test1.html', 'w', encoding='UTF-8') as output_file:
     #     output_file.write(t.text)
-    #
-    #
-    # if counter == 1:
-    #     break
+
