bdd8e726f380200c2d850bc0a6df8c21a841694a
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-29 22:29:45 +0300

    films

--- parser.py
+++ parser.py
@@ -26,19 +26,20 @@ r = get(main_url)
 soup = BeautifulSoup(r.text, 'lxml')
 _ = soup.find('div', {'class': 'tr_main_cats'})
 # Фильмы, Наше кино, Артхаус и авторское кино, DVD, HD, Мультсериалы соответсвенно
-category_list = ["viewforum.php?f=7", "viewforum.php?f=22", "viewforum.php?f=124", "viewforum.php?f=93",
+category_list_href = ["viewforum.php?f=7", "viewforum.php?f=22", "viewforum.php?f=124", "viewforum.php?f=93",
                  "viewforum.php?f=2198", "viewforum.php?f=4"]
 
-for i in category_list:
+for i in category_list_href:
     category_url = 'http://rutracker.org/forum/%s' % (i)
     c = get(category_url)
     soup = BeautifulSoup(c.text, 'lxml')
     _ = soup.find('table', {'class': 'forumline forum'})
     subcategory_list = _.findAll('h4', {'class': 'forumlink'})
     subcategory_list = [x.a.get('href') for x in subcategory_list]
-    # subcategory_list = list(map(get_href, subcategory_list))
     print(subcategory_list)
-    # for j in subcategory_list:
+    for j in subcategory_list:
+        if 'фильмы' in j:
+            print(j)
     #     subcategory_url = f'http://rutracker.org/forum/{j}'
     #     t = get(subcategory_url)
     #     soup = BeautifulSoup(t, 'lxml')
