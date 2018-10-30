2e937d6ca952a659d8245013b68c4ee8e9ece4d6
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-31 02:42:00 +0300

    f

--- parser2.py
+++ parser2.py
@@ -11,16 +11,12 @@ counter = 0
 s = soup('https://rutracker.org/forum/viewforum.php?f=187')
 x = s.findAll('div', {'class': 'torTopic'})
 for y in x:
-    z = y.find('a', {'class': 'torTopic bold tt-text'}).get('href')
-    print(z, end=';;;')
-    print()
-    counter += 1
-print(counter)
-# for y in x:
-#     print(y, end='---')
-#     print()
-# x = [y.span for y in x]
-# for y in x:
-#     if y is not None:
-#         y = y.text
-#         print(y, end=' ')
+    yy = y
+    yy = yy.span
+    if yy is not None and yy.text == '&radic':
+        z = y.find('a', {'class': 'torTopic bold tt-text'}).get('href')
+        print(z, end=';;;')
+        print()
+        counter += 1
+    print(counter)
+
