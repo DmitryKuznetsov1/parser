086232801c24051f9b42d5f66f0fcae8537df236
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-30 21:45:43 +0300

    f

--- parser.py
+++ parser.py
@@ -51,8 +51,8 @@ for i in category_list_href:
         z = subcategory_list[j][0]
         subcategory_url = 'http://rutracker.org/forum/%s' % z
         s = soup(subcategory_url)
-        x = s.find('div', {'class': 'bottom_info pad_2'}) # .findAll('b')
-        print(x) # x[1].text)
+       # x = s.find('div', {'class': 'bottom_info pad_2'}) # .findAll('b')
+        print(s) # x[1].text)
 
 
 
