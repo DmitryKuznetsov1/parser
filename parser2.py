a8c4cbb5e739dcccb951f45a2cd3c0f6bae5cd0a
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-31 02:10:30 +0300

    f

--- parser2.py
+++ parser2.py
@@ -10,8 +10,11 @@ def soup(url):
 s = soup('https://rutracker.org/forum/viewforum.php?f=187')
 x = s.findAll('div', {'class': 'torTopic'})
 for y in x:
-    print(y, end='---')
-    print()
+    z = y.find('a', {'class': 'torTopic bold tt-text'}).text
+    print(z)
+# for y in x:
+#     print(y, end='---')
+#     print()
 # x = [y.span for y in x]
 # for y in x:
 #     if y is not None:
