72a6aeca985d56f575c05d6b60aa96cf639c2121
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-30 19:06:29 +0300

    f

--- parser.py
+++ parser.py
@@ -41,7 +41,7 @@ for i in category_list_href:
     counter = -1
     for x in range(len(subcategory_list)):
         counter += 1
-        y = subcategory_list[x][1].lower()
+        y = subcategory_list[counter][1].lower()
         if ('фильмы' in y or 'кино' in y or 'кине' in y) and 'архив' not in y:
             subcategory_list.pop(counter)
             counter -= 1
--- parser2.py
+++ parser2.py
@@ -1,5 +1,4 @@
-a = [[1, 2], [3, 4], [4, 5], [5, 6]]
+a = [[1, 2], [3, 4], [5, 6], [7, 8]]
 for i in range(len(a)):
-    if i == 0:
-        a.pop([i])
-print(a)
+    y = a[i][1]
+print(len(a))
