2562dd9445f016dfa2f0659aa61ee50227270cd1
DmitryKuznetsov1 <dmitriy4ggl@gmail.com>
2018-10-30 22:15:22 +0300

    f

--- parser.py
+++ parser.py
@@ -49,10 +49,10 @@ for i in category_list_href:
         z = subcategory_list[j][0]
         subcategory_url = 'http://rutracker.org/forum/%s' % z
         u = get(subcategory_url).text
-        # s = BeautifulSoup(u, 'lxml')
+        s = BeautifulSoup(u, 'lxml')
         # x = s.find('div', {'class': 'bottom_info pad_2'}).findAll('b')
         # print(x[1].text)
-        print(u)
+        print(s)
 
 
     #     t = get(subcategory_url)
